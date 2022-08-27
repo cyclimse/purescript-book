92e64f7c153a29e386b5ce9ad6de6c6007c770b7
Andy Méry <andy.mery@pm.me>
2022-08-27 12:36:31 +0200

    feat(chapter2)

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,15 @@
 module Test.MySolutions where
 
 import Prelude
+
+import Data.Int (rem)
+import Data.Number as Math
+
+diagonal ∷ Number → Number → Number
+diagonal (a::Number) (b::Number) = a*a + b*b # Math.sqrt
+
+circleArea ∷ Number → Number
+circleArea (r::Number) = Math.pi * r * r
+
+leftoverCents ∷ Int → Int
+leftoverCents (n::Int) = rem n 100
